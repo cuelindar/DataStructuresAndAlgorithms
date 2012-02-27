96d12599f369eea5e1c64bf839da513c3cc1590f
Matthew Galbraith <cuelindar@gmail.com>
2012-02-26 18:30:31 -0800

    added Stack implementation that uses two queues internally

---
+++ src/org/hackmonkey/learning/java/algods/StackWithTwoQueues.java
@@ -0,0 +1,55 @@
+package org.hackmonkey.learning.java.algods;
+
+public class StackWithTwoQueues<T> implements IStack<T> {
+
+	IQueue<T> leftQueue;
+	IQueue<T> rightQueue;
+	
+	int itemCount;
+	
+	public StackWithTwoQueues(){
+		leftQueue = new Queue<T>();
+		rightQueue = new Queue<T>();
+		itemCount = 0;
+	}
+	
+	@Override
+	public void push(T item) {
+		if(itemCount > 0){
+			rightQueue.enqueue(item);
+			for(int i=0;i<leftQueue.getItemCount();i++){
+				rightQueue.enqueue(leftQueue.dequeue());
+			}
+			for(int i=0;i<rightQueue.getItemCount();i++){
+				leftQueue.enqueue(rightQueue.dequeue());
+			}
+		}
+		else{
+			leftQueue.enqueue(item);
+		}
+			
+	}
+
+	@Override
+	public T pop() {
+		if(itemCount == 0){
+			@SuppressWarnings("unchecked")
+			T ret = (T)null;
+			return ret;
+		}
+		
+		itemCount--;
+		return leftQueue.dequeue();
+	}
+
+	@Override
+	public int getItemCount() {
+		return itemCount;
+	}
+
+	@Override
+	public int getCurrentCapacity() {
+		return itemCount;
+	}
+
+}
